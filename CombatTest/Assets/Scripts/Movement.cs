036bbed4044b2328b96ad6f8d9796850c1bfa6d1
SebastianVP01 <124110054+SebastianVP01@users.noreply.github.com>
2023-02-06 21:44:11 -0600

    Codigo de pan

--- CombatTest/Assets/Scripts/Movement.cs
+++ CombatTest/Assets/Scripts/Movement.cs
@@ -6,18 +6,20 @@ public class Movement : MonoBehaviour
 {
     //hola
     //te amo guapa
+    //Yo tambien te quiero emi
     //aaaaah
     // Start is called before the first frame update
     private float horizontalInput;
     private float verticalInput;
     public float speedInput;
     // Update is called once per frame
+    
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
 
-        transform.Translate(Vector2.right * Time.deltaTime
-        * horizontalInput * speedInput);
+        transform.Translate(Vector2.right * Time.deltaTime * horizontalInput * speedInput);
+        
 
         
     }
