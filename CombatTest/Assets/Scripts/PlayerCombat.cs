879bd4853773e876f2c48fe0c9e0aba1aa5342a9
louieliet <101071655+louieliet@users.noreply.github.com>
2023-02-07 12:11:12 -0600

    updated

--- CombatTest/Assets/Scripts/PlayerCombat.cs
+++ CombatTest/Assets/Scripts/PlayerCombat.cs
@@ -2,38 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCombat : MonoBehaviour
-{
+public class PlayerCombat : MonoBehaviour{
+    
     public Transform attackPoint;
     public float attackRange = 0.5f;
     public LayerMask enemyLayers;
   
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         //Seleccionar la tecla de ataque
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
+        if(Input.GetKeyDown(KeyCode.Space)){
             Attack();
         }
     }
     //Funcion de ataque
-    void Attack()
-    {
+    void Attack(){
         //Animacion del ataque
         // ------------
-        //como me gusta Liz
         //Detect enemy
 
         //OverlapArea - Genera un rea para delimitar arma
        Collider2D[] hitEnemies=Physics2D.OverlapCircleAll(attackPoint.position, attackRange,enemyLayers);
-        foreach(Collider2D enemy in hitEnemies)
-        {
+        foreach(Collider2D enemy in hitEnemies){
             Debug.Log("We hit"+enemy.name);
         }
     }
     //visualizar area de ataque
     void OnDrawGizmosSelected() {
+        if (attackPoint == null) return;
         Gizmos.DrawWireSphere(attackPoint.position,attackRange);
         
     }
